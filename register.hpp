d3fa5184ba0ae714ced294cb0a66e57a7a1d6587
levi776 <levidr776@gmail.com>
2023-06-10 17:25:50 +0300

    TODO init

---
+++ register.hpp
@@ -0,0 +1,32 @@
+#ifndef REGISTER_HPP
+#define REGISTER_HPP
+
+//TODO: 
+/*
+#ifndef REGISTERS_HPP
+#define REGISTERS_HPP
+
+#include <string>
+
+class Registers
+{
+    int num_registers;
+    Registers() : num_registers(0) {}
+    Registers(Registers const& r) = delete;
+public:
+    
+    static Registers &instance() 
+    {
+        static Registers inst;
+        return inst;
+    }
+
+    std::string freshVar(std::string var_name = "", bool is_global = false)
+    {
+        std::string prefix = (is_global) ? "@" : "%";
+        if(var_name == "") var_name = "ig";
+        return prefix + var_name + "_" + std::to_string(num_registers++);
+    }
+};
+*/
+#endif
